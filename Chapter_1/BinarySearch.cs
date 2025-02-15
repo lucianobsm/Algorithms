6c83dd158b552767ef38c77a2eabfeb192e63e28
luciano <lucianobispods@gmail.com>
2025-02-15 16:27:46 -0300

    impl binary search

---
+++ Chapter_1/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithms.Chapter_1
+{
+    internal class BinarySearch
+    {
+        public static int? Search(int[] array, int value)
+        {
+            var start = 0;
+            var end = array.Length - 1;
+
+            while (start < end)
+            {
+                int middle = (start + end) / 2;
+
+                var kick = array[middle];
+
+                if (kick == value)
+                {
+                    return middle;
+                }
+
+                if (kick < value)
+                {
+                    start = middle + 1;
+                }
+                else
+                {
+                    end = middle - 1;
+                }
+            }
+
+            return null;
+        }
+    }
+}
