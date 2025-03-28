e1b3eb3d1b4e7faae287e71e4b3f308f8cb82aaa
luciano <lucianobispods@gmail.com>
2025-03-28 19:19:23 -0300

    impl quicksort

---
+++ Chapter_4/QuickSort.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithms.Chapter_4
+{
+    public class QuickSort
+    {
+        public static List<int> Sort(List<int> list)
+        {
+            if (list.Count <= 1) return list;
+
+            int pivot = list[0];
+            List<int> less = [];
+            List<int> greater = [];
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i] <= pivot)
+                {
+                    less.Add(list[i]);
+                }
+                else
+                {
+                    greater.Add(list[i]);
+                }
+            }
+
+            return [.. Sort(less), pivot, .. Sort(greater)];
+        }
+    }
+}
