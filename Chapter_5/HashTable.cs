295cd1610eb8544630c5ac588008f00d9caf622a
luciano <lucianobispods@gmail.com>
2025-03-28 19:20:10 -0300

    impl hash table/dictionary

---
+++ Chapter_5/HashTable.cs
@@ -0,0 +1,15 @@
+﻿namespace Algorithms.Chapter_5
+{
+    public class HashTable
+    {
+        public Dictionary<string, string> CreateDictionaryString()
+        {
+            var dict = new Dictionary<string, string>
+            {
+                { "test", "test" }
+            };
+            
+            return dict;
+        }
+    }
+}
